09bf2989c17641edb79ed1b1bfe53eb9a7d1de42
Davis <davis.benjamin41902@gmail.com>
2025-01-03 03:30:55 -0500

    breh

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import { buildApp } from "./app";
 async function start() {
   const app = await buildApp();
 
+  const port = process.env.PORT || "3000";
+  const portNumber = parseInt(port);
+
   try {
-    await app.listen({ port: 3000 });
-    console.log("Server running at http://localhost:3000");
+    await app.listen({ port: portNumber });
+    console.log(`Server running at http://localhost:${portNumber}`);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
